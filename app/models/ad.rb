40fd2b34dedcc22adba7f206e3a85308f4ee9d88
nicolas acard <acardnicolas@hotmail.com>
2016-02-24 18:47:19 +0100

    geocoder

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,10 +1,9 @@
 class Ad < ActiveRecord::Base
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
-geocoded_by :address
-after_validation :geocode, if: :address_changed?
-
-validates :flight_date, :price, :description, presence: true
-belongs_to :user
+  validates :flight_date, :price, :description, presence: true
+  belongs_to :user
 
 end
