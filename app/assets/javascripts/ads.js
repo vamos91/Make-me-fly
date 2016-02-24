3eb6acbb6253d7490f7e0e73c920d81efbce8237
Michael TURCAN <michael13013@gmail.com>
2016-02-24 18:11:12 +0100

    24-02-2016 au soir

--- app/assets/javascripts/ads.js
+++ app/assets/javascripts/ads.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  $('.card').on('click', function() {
+    window.location.href = $(this).data('url');
+  });
+});
