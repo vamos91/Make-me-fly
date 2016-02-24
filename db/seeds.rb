d5efe67017270c5ec718170b6eeeac2836751172
Michael TURCAN <michael13013@gmail.com>
2016-02-24 18:57:48 +0100

    geocoder initializer + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,12 @@ User.destroy_all
 Ad.destroy_all
 
 10.times do
-  address = Faker::Address.street_address + ' ' + Faker::Address.city
-
   password = Faker::Internet.password(8)
   user = User.create!(name: Faker::Name.last_name, email: Faker::Internet.email, password: password)
   user.save
 
   10.times do
+    address = Faker::Address.street_address
     annonce = Ad.create(description: Faker::Lorem.paragraph, price: Faker::Number.number(2), flight_date: Faker::Date.between(2.days.ago, Date.today), flight_time: Faker::Number.number(2), address: address)
     annonce.user = user
     annonce.save!
