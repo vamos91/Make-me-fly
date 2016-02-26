2dd1be2684fee737245e7a383b7aec9d0a494b01
david parienti <freshideababy@gmail.com>
2016-02-26 16:34:27 +0100

    suite

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "michael13013@gmail.com"
   layout 'mailer'
 end
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+class UserMailerPreview < ActionMailer::Preview
+  def welcome
+    user = User.first
+    UserMailer.welcome(user)
+  end
+end
+
+
+
