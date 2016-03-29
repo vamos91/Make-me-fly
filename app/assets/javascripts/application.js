2912e6567855944359eac428ac90f8b3ba6016f2
nicolas acard <acardnicolas@hotmail.com>
2016-03-29 14:00:19 +0200

    home fix counter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,19 @@ $(function(){
     verticalOffset: 40
   });
 });
+
+
+
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
