58e239e9ca1d5be2681b7252e01c20635a55501f
nicolas acard <acardnicolas@hotmail.com>
2016-04-19 16:21:09 +0200

    stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
 
 def create
   @ad = Ad.find(params[:ad_id])
+  authorize @ad
   order = Order.create!(ad_sku: @ad.id, amount: @ad.price, status: 'pending')
 
   redirect_to new_order_payment_path(order)
