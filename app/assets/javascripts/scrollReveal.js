4fb1f759d7814360a5af5f266e13c19e8277b04c
nicolas acard <acardnicolas@hotmail.com>
2016-05-04 19:28:23 +0200

    design

--- app/assets/javascripts/scrollReveal.js
+++ app/assets/javascripts/scrollReveal.js
@@ -4,4 +4,7 @@ sr.reveal('h1');
 sr.reveal('.banner.h1', { delay: 600, reset:false });
 sr.reveal('.card', { origin:"bottom",
   distance : '100px' , duration: 1000, reset:false });
-sr.reveal('.wrapper-grey', { origin:"left", move: '50px', duration: 1000, reset:true })
+sr.reveal('.wrapper-grey', { origin:"left", distance: '500px', duration: 2000, reset:false })
+sr.reveal('.wrapper-feature', { origin: "right", distance: '500px', duration: 1000, reset: false})
+sr.reveal('.compteur', { origin: "left", distance: '50px', duration: 1000, reset: false})
+sr.reveal('.formulaire', { origin: "bottom", distance: '50px', duration: 1000, reset: false})
