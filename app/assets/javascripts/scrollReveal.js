06e37f92b3fdcddd45f5fc4d4c46418626f55d9e
nicolas acard <acardnicolas@hotmail.com>
2016-06-04 13:29:55 +0200

    googlmap fix

--- app/assets/javascripts/scrollReveal.js
+++ app/assets/javascripts/scrollReveal.js
@@ -4,8 +4,8 @@ sr.reveal('h1');
 sr.reveal('.banner.h1', { delay: 600, reset:false });
 sr.reveal('.reveal .card', { origin:"bottom",
   distance : '100px' , duration: 1000, reset:false });
-sr.reveal('.wrapper-grey', { origin:"left", distance: '500px', duration: 2000, reset:false })
-sr.reveal('.wrapper-feature', { origin: "right", distance: '500px', duration: 1000, reset: false})
+sr.reveal('.wrapper-grey', { origin:"bottom", distance: '50px', duration: 2000, reset:false })
+sr.reveal('.wrapper-feature', { origin: "bottom", distance: '50px', duration: 1000, reset: false})
 sr.reveal('.compteur', { origin: "left", distance: '50px', duration: 1000, reset: false})
 sr.reveal('.formulaire', { origin: "bottom", distance: '50px', duration: 1000, reset: false})
 sr.reveal('.counter')
