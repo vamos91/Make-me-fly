dca6cda6edda7679c03c42cb59abaeb546d0caf6
nicolas acard <acardnicolas@hotmail.com>
2016-06-04 16:08:07 +0200

    reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,22 @@
 class UsersController < ApplicationController
 
  def index
-  users = User.all
-  @pilote = user.pilote
-  authorize @pilote
+  # users = User.all
+  # @pilote = users.pilote
+  # authorize @pilote
  end
 
  def show
    @user = User.find(params[:id])
+   authorize @user
  end
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_param)
-  # end
 
 
 
 
   # private
   # def user_param
-  #   params.require(:user).permit(:name, :pilote)
+  #   params.require(:users).permit(:name, :pilote)
   # end
 
 
