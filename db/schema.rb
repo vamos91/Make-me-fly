0eaa7e5fa95fc9a1bea43d31d7d649d3120c8d1e
nicolas acard <acardnicolas@hotmail.com>
2016-11-16 14:59:40 +0100

    messaging update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418195019) do
+ActiveRecord::Schema.define(version: 20161116135626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20160418195019) do
   end
 
   add_index "ads", ["user_id"], name: "index_ads_on_user_id", using: :btree
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string   "status"
