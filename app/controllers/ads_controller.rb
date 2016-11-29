3cf8c516e09c48c691ece8530b934e57d1df806d
nicolas acard <acardnicolas@hotmail.com>
2016-11-29 14:44:32 +0100

    météo fix

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,6 +1,6 @@
 class AdsController < ApplicationController
   skip_before_action :authenticate_user!
-  #before_action :set_forecast, only: [:show]
+  before_action :set_forecast, only: [:show]
 
 
  def index
@@ -119,6 +119,14 @@ class AdsController < ApplicationController
 
   def conversation_params
     params.permit(:sender_id, :recipient_id, :ad_id)
+  end
+
+  def set_forecast
+    @forecast = Forecast.find(params[:id])
+  end
+
+  def forecast_params
+    params.require(:forecast).permit(:lat, :lng)
   end
 
 end
