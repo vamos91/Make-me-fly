1573747af9f40eb55684920debaee7f0b7f1ae5f
nicolas acard <acardnicolas@hotmail.com>
2016-12-19 15:31:41 +0100

    hometown ad done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,19 @@ class PagesController < ApplicationController
   skip_after_action :verify_authorized
 
   def home
-
     ad = Ad.count
     @pilote_counter = User.where("pilote = ?", true).count
     @passager_counter = User.where("pilote = ?", false).count
     #@ads = Ad.all.sort[ad-9..ad].reverse
     #@ads = Ad.order(id: :desc).limit(9)
-    @ads = Ad.where("flight_date >= ?", DateTime.now).order(id: :desc).limit(9)
-  #si la date du vol est supérieur à la date.now on incremente un conmpteur de 1 dans une boucle
+    if user_signed_in?
+      @ads = Ad.near(current_user.hometown, 150, :unit => :km)
+    else
+      @ads = Ad.where("flight_date >= ?", DateTime.now).order(id: :desc).limit(9)
+    end
 
+
+  #si la date du vol est supérieur à la date.now on incremente un conmpteur de 1 dans une boucle
     @ads_by_date_counter = Ad.where("flight_date >= ?", DateTime.now)
     @ads_counter = 0
     @ads_by_date_counter.each do |c|
@@ -21,8 +25,6 @@ class PagesController < ApplicationController
       end
     end
      return @ads_counter
-
-
-
   end
+
 end
