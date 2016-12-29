1c6df971100e3f44c71238e887bc4a69c75666c8
nicolas acard <acardnicolas@hotmail.com>
2016-12-29 12:24:40 +0100

    optimisation home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
 
   def home
     ad = Ad.count
-    @pilote_counter = User.where("pilote = ?", true).count
-    @passager_counter = User.where("pilote = ?", false).count
+    #@pilote_counter = User.where("pilote = ?", true).count
+    #@passager_counter = User.where("pilote = ?", false).count
     #@ads = Ad.all.sort[ad-9..ad].reverse
     #@ads = Ad.order(id: :desc).limit(9)
     if user_signed_in?
@@ -19,16 +19,6 @@ class PagesController < ApplicationController
       @ads = Ad.where("flight_date >= ?", DateTime.now).order(id: :desc).limit(9)
     end
 
-
-  #si la date du vol est supérieur à la date.now on incremente un conmpteur de 1 dans une boucle
-    @ads_by_date_counter = Ad.where("flight_date >= ?", DateTime.now)
-    @ads_counter = 0
-    @ads_by_date_counter.each do |c|
-      if c.flight_date >= DateTime.now
-        @ads_counter = @ads_counter + 1
-      end
-    end
-     return @ads_counter
   end
 
 end
