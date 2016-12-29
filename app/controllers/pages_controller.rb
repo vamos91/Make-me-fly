ca4660ce6c3fcf9b75541acf616788f21b312203
nicolas acard <acardnicolas@hotmail.com>
2016-12-29 12:29:40 +0100

    optimisation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   skip_after_action :verify_authorized
 
   def home
-    ad = Ad.count
+    #ad = Ad.count
     #@pilote_counter = User.where("pilote = ?", true).count
     #@passager_counter = User.where("pilote = ?", false).count
     #@ads = Ad.all.sort[ad-9..ad].reverse
@@ -19,6 +19,16 @@ class PagesController < ApplicationController
       @ads = Ad.where("flight_date >= ?", DateTime.now).order(id: :desc).limit(9)
     end
 
+
+  #si la date du vol est supérieur à la date.now on incremente un conmpteur de 1 dans une boucle
+    # @ads_by_date_counter = Ad.where("flight_date >= ?", DateTime.now)
+    # @ads_counter = 0
+    # @ads_by_date_counter.each do |c|
+    #   if c.flight_date >= DateTime.now
+    #     @ads_counter = @ads_counter + 1
+    #   end
+    # end
+    #  return @ads_counter
   end
 
 end
