1478b38c52e06ac66cc3a7148a919b5310e9bde6
nicolas acard <acardnicolas@hotmail.com>
2017-01-17 10:30:40 +0100

    facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
     before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_filter :after_sign_up_path_for, :unless => :devise_controller?
+
 
   protect_from_forgery with: :exception
 
@@ -20,7 +22,9 @@ before_action :authenticate_user!
   end
 
 
-
+  def after_sign_up_path_for(resource_or_scope)
+    "http://www.google.com" # <- Path you want to redirect the user to.
+  end
 
 
   def configure_permitted_parameters
@@ -33,4 +37,5 @@ before_action :authenticate_user!
    # devise_parameter_sanitizer.for(:sign_up) << :user
 
   end
+
 end
