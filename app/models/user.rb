738770ef1c47435cf03007342aaf52957d5e7f61
nicolas acard <acardnicolas@hotmail.com>
2017-01-20 21:12:53 +0100

    fix db problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :hometown, presence: true
+  #validates :hometown, presence: true
   validates :name, presence: true
   #
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170117224545) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -130,7 +129,6 @@ ActiveRecord::Schema.define(version: 20170117224545) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
-
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
