46c6954ca792482f9d6469dab2bda02c69a89dcb
nicolas acard <acardnicolas@hotmail.com>
2017-01-29 17:25:15 +0100

    email design

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,29 +12,29 @@ User.destroy_all
 acard = User.create!(name: "acard", email: "acardnicolas@hotmail.com", password:"password", admin: true)
 Ad.destroy_all
 
-5.times do
+# 5.times do
 
-  password = Faker::Internet.password(8)
-  user = User.create!(name: Faker::Name.last_name, email: Faker::Internet.email, password: password, pilote: false)
-  user.picture = Faker::Avatar.image(user.name.parameterize, "50x50")
-  puts user.picture
-  user.save
+#   password = Faker::Internet.password(8)
+#   user = User.create!(name: Faker::Name.last_name, email: Faker::Internet.email, password: password, pilote: false)
+#   user.picture = Faker::Avatar.image(user.name.parameterize, "50x50")
+#   puts user.picture
+#   user.save
 
-  1.times do
-    #address = Faker::Address.street_address +', Paris'
-    address = "place de la bastille, Paris"
-    annonce = Ad.create(description: Faker::Lorem.paragraph, price: Faker::Number.number(2), flight_date: Date.today, flight_time: Faker::Number.number(2), address: address)
-    annonce.user = user
-    article = Article.create(title: Faker::Book.title, description: Faker::Lorem.paragraph, video_url: Faker::Internet.url)
-    article.user = user
-    image_url = Faker::Avatar.image(('Vol depuis ' + Faker::Address.city).parameterize, "300x150", "jpg")
-    annonce.remote_photo_url = image_url
-    annonce.save!
-    puts address
-  end
-end
-
-
+#   1.times do
+#     #address = Faker::Address.street_address +', Paris'
+#     address = "place de la bastille, Paris"
+#     annonce = Ad.create(description: Faker::Lorem.paragraph, price: Faker::Number.number(2), flight_date: Date.today, flight_time: Faker::Number.number(2), address: address)
+#     annonce.user = user
+#     article = Article.create(title: Faker::Book.title, description: Faker::Lorem.paragraph, video_url: Faker::Internet.url)
+#     article.user = user
+#     image_url = Faker::Avatar.image(('Vol depuis ' + Faker::Address.city).parameterize, "300x150", "jpg")
+#     annonce.remote_photo_url = image_url
+#     annonce.save!
+#     puts address
+#   end
+# end
 
 
-puts "#{User.count} user, #{Ad.count} et #{Article.count} annonces crées !"
+
+
+# puts "#{User.count} user, #{Ad.count} et #{Article.count} annonces crées !"
