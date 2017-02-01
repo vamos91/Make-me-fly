d548bbe76f353ce1ddc3963702e6074a2b43797a
nicolas acard <acardnicolas@hotmail.com>
2017-02-01 13:56:04 +0100

    banner user disconnect

---
+++ app/assets/javascripts/app/banner-home-user-disconnect.js
@@ -0,0 +1,10 @@
+var banner = $('#banner-user-disconnect').hide();
+setTimeout(toto, 3000);
+function toto(){
+  $('#banner-user-disconnect').fadeIn();
+}
+
+document.getElementById("close-button").onclick = function() {close_banner()};
+function close_banner(){
+  document.getElementById("banner-user-disconnect").remove();
+}
