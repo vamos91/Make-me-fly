beb85ed92e0ce70657aeaf7bc8db28a17afccb5d
nicolas acard <acardnicolas@hotmail.com>
2017-02-02 13:05:41 +0100

    javacript banner fix

---
+++ app/assets/javascripts/app/banner.js
@@ -0,0 +1,15 @@
+var banner = $('#banner-user-disconnect').hide();
+
+function toto(){
+  $('#banner-user-disconnect').fadeIn();
+}
+
+setTimeout(toto, 3000);
+
+function close_banner(){
+  $("#banner-user-disconnect").remove();
+};
+
+
+
+
