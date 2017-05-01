22e77e0a8d0b75e30b31c338bdcc15a46da8ee23
nicolas acard <acardnicolas@hotmail.com>
2017-05-02 01:21:18 +0200

    design user pilote

--- app/assets/javascripts/app/navbar.js
+++ app/assets/javascripts/app/navbar.js
@@ -1,7 +1,7 @@
 (function(){
     $(window).scroll(function() {
 
-        if($(this).scrollTop() > 750) {
+        if($(this).scrollTop() > 150) {
             $('.navbar-wagon-root').addClass('nav--sticky');
         } else {
             $('.navbar-wagon').removeClass('nav--sticky');
