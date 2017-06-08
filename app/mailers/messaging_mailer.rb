99058c7e5cc3565c96cd9980a1bf5d06f0cf0148
nicolas acard <acardnicolas@hotmail.com>
2017-06-08 20:10:32 +0200

    email fix

--- app/mailers/messaging_mailer.rb
+++ app/mailers/messaging_mailer.rb
@@ -2,7 +2,6 @@ class MessagingMailer < ApplicationMailer
   def messaging(message)
     conversation = Conversation.find(message.conversation_id)
     @body_message = message.body
-    binding.pry
     @sender = User.find(conversation.sender_id)
     @recipient = User.find(conversation.recipient_id)
     mail(to: @recipient.email, subject: "MakeMeFly : Vous avez recus un message de #{@sender.name}")
