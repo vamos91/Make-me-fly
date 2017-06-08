87f89d2e05f69c27abc790a80c8f95705b29a891
nicolas acard <acardnicolas@hotmail.com>
2017-06-08 19:42:25 +0200

    email fix

--- app/assets/javascripts/app/comment.js
+++ app/assets/javascripts/app/comment.js
@@ -1,16 +1,29 @@
-// commentaire article
-$('#show-comment').hide();
-$('#unbutton-comment').hide();
-function show(){
-  $('#show-comment').fadeIn().show();
-  $('#unbutton-comment').show();
-  $('#hide-comment').hide();
-  $('#button-comment').hide();
-};
+ $('.show-comment').hide();
+// $('#unbutton-comment').hide();
+// function show(){
+//   $('#show-comment').slideDown().show();
+//   $('#unbutton-comment').show();
+//   $('#hide-comment').hide();
+//   $('#button-comment').hide();
+// };
 
-function reduce(){
-  $('#show-comment').hide();
-  $('#unbutton-comment').hide();
-  $('#hide-comment').fadeIn().show();
-  $('#button-comment').show();
-}
+// function reduce(){
+//   $('#show-comment').hide();
+//   $('#unbutton-comment').hide();
+//   $('#hide-comment').slideToggle().show();
+//   $('#button-comment').show();
+// }
+
+// $(document).ready(function() {
+//     $('.hide-comments').click(function(e) {
+//       e.preventDefault();
+//       $(this).parent().toggle();
+//     });
+// });
+
+// $(document).ready(function() {
+//     $('.show-comments').click(function(e) {
+//       e.preventDefault();
+//       $(this).parent().show();
+//     });
+// });
