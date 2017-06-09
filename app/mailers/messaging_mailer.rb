fd6a871a384d67fc108699d51d863b7c377c7156
nicolas acard <acardnicolas@hotmail.com>
2017-06-09 14:09:04 +0200

    messagon system

--- app/mailers/messaging_mailer.rb
+++ app/mailers/messaging_mailer.rb
@@ -1,7 +1,10 @@
 class MessagingMailer < ApplicationMailer
   def messaging(message)
+    #binding.pry
     conversation = Conversation.find(message.conversation_id)
     @body_message = message.body
+    #si message.user_id == current_user
+    ##alors @sender = current_user
     @sender = User.find(conversation.sender_id)
     @recipient = User.find(conversation.recipient_id)
     mail(to: @recipient.email, subject: "MakeMeFly : Vous avez recus un message de #{@sender.name}")
