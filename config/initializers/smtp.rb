75e8ca39dfc4eb3043a3ba50b94cfdc3a52c2f0d
nicolas acard <acardnicolas@hotmail.com>
2017-07-05 00:11:21 +0200

    email fix

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.postmarkapp.com',
-  port: '25',
-  domain: 'postmarkapp.com',
-  user_name: ENV['POSTMARK_API_TOKEN'],
-  password: ENV['POSTMARK_API_TOKEN'],
-  authentication: :cram_md5,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.postmarkapp.com',
+#   port: '25',
+#   domain: 'postmarkapp.com',
+#   user_name: ENV['POSTMARK_API_TOKEN'],
+#   password: ENV['POSTMARK_API_TOKEN'],
+#   authentication: :cram_md5,
+#   enable_starttls_auto: true
+# }
